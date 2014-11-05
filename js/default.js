485e3a757cab339fa829860a695bcb731ca4d48e
HOLOSAMPLES <hanlong@notmatter.com>
2014-11-05 14:43:19 +0800

    holoide commit

--- js/default.js
+++ js/default.js
@@ -7,6 +7,53 @@
  Init();
  animate();
 
+
+function Init(){
+        scene = new THREE.Scene();
+  
+       //setup camera
+ 		camera = new LeiaCamera();
+        camera.position.copy(_camPosition);
+        camera.lookAt(_tarPosition);
+        scene.add(camera);
+  
+       //setup rendering parameter
+ 		renderer = new LeiaWebGLRenderer({
+         antialias:true, 
+ 		renderMode: _renderMode, 
+		shaderMode: _nShaderMode,
+		devicePixelRatio: 1 
+        } );
+ 		renderer.Leia_setSize( windowWidth, windowHeight );
+ 		document.body.appendChild( renderer.domElement );
+  
+       //add object to Scene
+       LEIA_setBackgroundPlane('resource/vanishingpt5.png');
+       LEIA_setCenterPlane('resource/oval2.png');
+       readSTLs('resource/cubes.stl');
+  
+        //add Light
+ 		var xl = new THREE.DirectionalLight( 0x555555 );
+ 		xl.position.set( 1, 0, 2 );
+ 		scene.add( xl );
+ 		var pl = new THREE.PointLight(0x111111);
+ 		pl.position.set(-20, 10, 20);
+ 		scene.add(pl);
+ 		var ambientLight = new THREE.AmbientLight(0x111111);	
+ 		scene.add(ambientLight);
+ }
+
+ function animate() 
+ {
+ 	requestAnimationFrame( animate );
+    if (newMeshReady) {
+               
+       mesh1.rotation.y = 1.032 * Date.now() * 0.001;
+    }
+    renderer.setClearColor(new THREE.Color().setRGB(1.0, 1.0, 1.0)); 
+	renderer.Leia_render(scene, camera,undefined,undefined,_holoScreenScale,_camFov,_messageFlag);
+ }
+
 function readSTLs(filename) {
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function () {
@@ -63,48 +110,3 @@ function LEIA_setCenterPlane(filename, aspect){
 	LEIA_centerPlane.position.z = 0;
 	scene.add(LEIA_centerPlane);
 }
-function Init(){
-        scene = new THREE.Scene();
-  
-       //setup camera
- 		camera = new LeiaCamera();
-        camera.position.copy(_camPosition);
-        camera.lookAt(_tarPosition);
-        scene.add(camera);
-  
-       //setup rendering parameter
- 		renderer = new LeiaWebGLRenderer({
-         antialias:true, 
- 		renderMode: _renderMode, 
-		shaderMode: _nShaderMode,
-		devicePixelRatio: 1 
-        } );
- 		renderer.Leia_setSize( windowWidth, windowHeight );
- 		document.body.appendChild( renderer.domElement );
-  
-       //add object to Scene
-       LEIA_setBackgroundPlane('resource/vanishingpt5.png');
-       LEIA_setCenterPlane('resource/oval2.png');
-       readSTLs('resource/cubes.stl');
-  
-        //add Light
- 		var xl = new THREE.DirectionalLight( 0x555555 );
- 		xl.position.set( 1, 0, 2 );
- 		scene.add( xl );
- 		var pl = new THREE.PointLight(0x111111);
- 		pl.position.set(-20, 10, 20);
- 		scene.add(pl);
- 		var ambientLight = new THREE.AmbientLight(0x111111);	
- 		scene.add(ambientLight);
- }
-
- function animate() 
- {
- 	requestAnimationFrame( animate );
-    if (newMeshReady) {
-               
-       mesh1.rotation.y = 1.032 * Date.now() * 0.001;
-    }
-    renderer.setClearColor(new THREE.Color().setRGB(1.0, 1.0, 1.0)); 
-	renderer.Leia_render(scene, camera,undefined,undefined,_holoScreenScale,_camFov,_messageFlag);
- }
